4a6661cba9a34d13ad92a99e7e3322879eb11ad0
Jordan Rivera <jordan.rivera34@gmail.com>
2019-03-25 17:09:08 -0400

    pt.2

--- db/migrate/02_create_rosters.rb
+++ db/migrate/02_create_rosters.rb
@@ -1,8 +1,9 @@
 class CreateRosters < ActiveRecord::Migration[5.2]
     def change
         create_table :rosters do |t|
-            t.string :user
-            t.string :anime_char
+            t.string :name
+            t.integer :user_id
+            t.integer :anime_char_id
         end
     end
 end
